14b366ddb16f92f310982be567495dfa4c629c86
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-01-11 20:06:45 -0600

    changed the seeds, schema, and hopefully fixed the git ignore

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,14 @@ CREATE TABLE events
 (
 	id int NOT NULL AUTO_INCREMENT primary key,
 	account_name varchar(255) NOT NULL,
-	event_name varchar(255) NOT NULL,
-	event_location varchar(255) NOT NULL,
-	event_time datetime
+	title varchar(255) NOT NULL,
+	organizer varchar(255) NOT NULL,
+	short_desc TEXT NOT NULL,
+	full_desc TEXT NOT NULL,
+	event_date datetime,
+	event_time datetime,
+	event_address VARCHAR(350) NOT NULL,
+	eventLinkPic VARCHAR(1000) NOT NULL,
+	eventCategoryOne VARCHAR(20) NOT NULL,
+	eventCategoryTwo VARCHAR(20) NOT NULL
 );
