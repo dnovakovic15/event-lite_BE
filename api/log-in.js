b7421dfaa8c11db8af2be3d8153f9b3e0f553565
dnovakovic15 <dnovakovic21@yahoo.com>
2018-01-12 14:35:50 -0600

    Sign-Up Added

--- api/log-in.js
+++ api/log-in.js
@@ -1,17 +1,21 @@
 let con = require('../config/connection.js');
+let bcrypt = require('bcrypt');
 
 function validateUser(userName, password, cb, res){
-    let sql = 'SELECT password FROM users'
+    let sql = `SELECT password FROM users WHERE name="${userName}"`
 
     con.query(sql, function (err, result) {
         if (err) throw err;
 
-        if(result[0].password == password){
+        let hash = result[0].password;
+
+        bcrypt.compare(password, hash, function(err, doesMatch){
+        if (doesMatch){
             cb(true, res, userName);
+        }else{
+            cb(false, res);
         }
-        else{
-           cb(false, res);
-        }
+        });
     });
 }
 
