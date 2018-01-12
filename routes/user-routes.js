dadd46dd14ab4d01d7834e8e894e9d059bea21d5
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-01-12 13:16:17 -0600

    installed bcrypt npm, updated log-in, updated user routes, created sign up

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,40 @@
 var jwt = require('jsonwebtoken');
 var users = require("../api/log-in.js");
+var newUser = require("../api/sign-up.js");
 
 module.exports = function(app) {
-  app.get("/api/login/:user/:password", function(req, res) {
-    users('darko', 'abc123', cb, res);
+  app.post("/api/login", function(req, res) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    console.log(req.body.username);
+    users(req.body.username, req.body.password, cb, res);
+  });
+
+  app.post("/api/signup", function(req, res) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    newUser(req.body.username, req.body.password, cb, res);
   });
 
   app.get("/userAccount/:token", function(req, res){
