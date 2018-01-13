99ad800c23fb944ed81f29e5394e6ee2bd7396eb
Will Vongkoth <avongkoth92@u.northwestern.edu>
2018-01-12 22:32:42 -0600

    changed request to req

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -29,7 +29,7 @@ module.exports = function(app) {
 
   app.post("/api/insertEvent", function(req, res) {
     let dateTime = req.body.date + ' ' + req.body.time;
-    db.selectOne('root', req.body.title, req.body.organizer, req.body.short, req.body.long, dateTime, req.body.address, req.body.pic, request.body.category1, request.body.category2);
+    db.selectOne('root', req.body.title, req.body.organizer, req.body.short, req.body.long, dateTime, req.body.address, req.body.pic, req.body.category1, req.body.category2);
   });
 
   //Implement Later
